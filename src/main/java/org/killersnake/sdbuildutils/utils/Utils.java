e725b1322496125bad03e53b6bbb2e33f31d340f
PallavJain01 <pallavjainofficial@gmail.com>
2025-12-29 17:59:41 +0530

    add plugin namespace to chat message

--- src/main/java/org/killersnake/sdbuildutils/utils/Utils.java
+++ src/main/java/org/killersnake/sdbuildutils/utils/Utils.java
@@ -6,7 +6,7 @@ import org.bukkit.Bukkit;
 public class Utils {
 
 	public static void messagePlayer(String name, String message) {
-		Bukkit.getPlayer(name).sendMessage(Component.text(message));
+		Bukkit.getPlayer(name).sendMessage(Component.text("[SDBuildUtils] " + message));
 	}
 
 }
