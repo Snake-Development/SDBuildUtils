06187b73c6467c1f4d12ba821a0c5535868530e7
PallavJain01 <pallavjainofficial@gmail.com>
2025-12-29 19:49:24 +0530

    add item blacklist feature for selection items

--- src/main/java/org/killersnake/sdbuildutils/utils/Utils.java
+++ src/main/java/org/killersnake/sdbuildutils/utils/Utils.java
@@ -2,11 +2,18 @@ package org.killersnake.sdbuildutils.utils;
 
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import java.util.Arrays;
 
 public class Utils {
 
 	public static void messagePlayer(String name, String message) {
 		Bukkit.getPlayer(name).sendMessage(Component.text("[SDBuildUtils] " + message));
 	}
-
+	public static boolean isBlacklisted(Material material) {
+		final Material[] blacklistedMaterials = {
+			Material.SCULK_SHRIEKER
+		};
+		return Arrays.stream(blacklistedMaterials).anyMatch(mat -> mat == material);
+	}
 }
